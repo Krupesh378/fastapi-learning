377595b66e390e466714d3baf05c3a50cc02b0bf
krupesh <krupesh.patel@acquaintsoft.com>
2025-05-22 16:23:27 +0530

    fix: remvoed sync on database configuration and added async configuration

--- async_app/database.py
+++ async_app/database.py
@@ -1,19 +1,20 @@
-"""This module has been handle the database connectiviy."""
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker
+)
+from sqlalchemy.orm import declarative_base
 
+DATABASE_URL = "postgresql+asyncpg://krupesh:krupesh123@localhost:5432/user"
 
-DATABASE_URL = "postgresql+asyncpg://kruepsh:krupesh123@localhost:5432/learn"
-
-engine = create_async_engine(DATABASE_URL)
-AsyncSessionLocal = sessionmaker(
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
 
 Base = declarative_base()
-
 
 async def get_session():
     async with AsyncSessionLocal() as session:
