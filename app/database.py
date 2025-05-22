2048cb322ec6d5c49e058eff7604c6c243a4325f
krupesh <krupesh.patel@acquaintsoft.com>
2025-05-22 12:02:46 +0530

    compose: created the docker compose file for easly configuration

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://krupesh:krupesh123@localhost:5432/learn"
+DATABASE_URL = "postgresql://kruepsh:krupesh123@db:5432/learn"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
